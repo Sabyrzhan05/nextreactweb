bf1ae94d672abb49971e497beb8cbcb07dde82f2
Sabyrzhan05 <ruslanovilyas05@gmail.com>
2025-11-13 18:09:59 +0500

    gdmn t

--- src/app/Login/page.tsx
+++ src/app/Login/page.tsx
@@ -4,7 +4,7 @@ export default function Login(){
     return(
         
         <main className="flex min-h-screen m-auto justify-center items-center p-6">
-            <RegistrationForm />
+            <h1>Here is no registration for mthrfucker</h1>
         
         </main>
     );
